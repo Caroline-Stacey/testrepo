55cc88661a48f810ef34d2383bffdbfd624d534a
Caroline-Stacey <71742976+Caroline-Stacey@users.noreply.github.com>
2020-09-22 22:31:37 -0400

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,20 @@
+text = "1\t2\n3"
+print (text)
+
+
+a, b = 1, 2
+print(a)
+print(b)
+
+
+print("\n\n\n end")
+
+
+#get the name from the user
+value = input("Please enter your name ")
+print (value)
+
+age = input("Enter your age ")
+future_age = int(age) + 10
+print (future_age)
+print (f"In 10 years you will be {future_age}")
